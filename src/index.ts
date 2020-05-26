4bb216aafcbe039044217f8e31c93f6882c0f113
Skatole <r.bencelaszlo9511@gmail.com>
2020-05-26 18:30:16 +0200

    Half of Swagger, and index.ts done

--- src/index.ts
+++ src/index.ts
@@ -1,30 +1,30 @@
 import * as express from 'express';
-import { Request, Response } from 'express';
 import * as createMiddleware from 'swagger-express-middleware';
 import { SwaggerMiddleware } from 'swagger-express-middleware';
-import { database } from './lib/database';
+import { Application } from 'express';
+import * as swaggerUi from 'swagger-ui-express';
+import * as swaggerDocument from '../config/swagger.json';
+import { router } from '../src/app/routers/flat';
 
-const app = express();
-const { PORT = 3000 } = process.env;
+const app: Application = express();
+app.use(express.json());
 
 createMiddleware('config/swagger.json', app, (err, middleware: SwaggerMiddleware) => {
   if (err) {
     console.error(err);
   }
 
+  app.use('/swagger', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
+  app.use(router);
+  
   app.use(
     middleware.metadata(),
     middleware.CORS(),
     middleware.parseRequest(),
-    middleware.validateRequest()
-  );
-
-  app.get('/', async (req: Request, res: Response) => {
-    res.json({
-      message: 'hello world',
-    });
-  });
-
+    );
+    
+    const { PORT = 3000 } = process.env;
+  
   app.listen(PORT, () => {
     console.log(`server started at http://localhost:${PORT}`);
   });
